c4f40782018ec8e8672ee0a96488c453bbc54875
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-05 17:12:43 +0300

    Updates

--- opcode.c
+++ opcode.c
@@ -4,7 +4,7 @@ static instruction_t ops[] = {
 	{"push", _push}, {"pall", _pall}, {"pint", _pint}, {"pop", _pop},
 	{"swap", _swap}, {"add", _add}, {"nop", NULL}, {"sub", _sub}, {"div", _div},
 	{"mul", _mul}, {"mod", _mod}, {"pchar", _pchar}, {"pstr", _pstr},
-	{"rotl", _rotl}, {"rotr", _rotr}, {"stack", _stack}, {"stack", _queue},
+	{"rotl", _rotl}, {"rotr", _rotr}, {"stack", _stack}, {"queue", _queue},
 	{NULL, NULL}
 };
 
