f70b5a86f58d5c097ac00ef46b718a22c6ad3050
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-05 13:28:53 +0300

    Push and pall opcodes

---
+++ push_and_pall.c
@@ -0,0 +1,56 @@
+#include "monty.h"
+
+/**
+ * _pall - prints a list of stacks
+ * @list: pointer to a stack_t lists
+ * @count: number of line number
+ *
+ * Return: no return
+ */
+void _pall(stack_t **list, unsigned int count)
+{
+	stack_t *node, *head;
+	(void) count;
+
+	if (!list || !(*list))
+		return;
+	node = head = *list;
+
+	while (node)
+	{
+		fprintf(stdout, "%d\n", node->count);
+		node = node->next;
+	}
+}
+
+/**
+ * _push - adds a node to a list of stacks
+ * @list: pointer to a stack_t lists
+ * @count: number of line number
+ *
+ * Return: no return
+ */
+void _push(stack_t **list, unsigned int count)
+{
+	stack_t *node = NULL;
+	(void) count;
+
+	node = malloc(sizeof(stack_t));
+	if (!node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_stack(list);
+		exit(EXIT_FAILURE);
+	}
+
+	node->prev = node->next = NULL;
+
+	if (!(*list))
+		(*list) = node;
+	else
+	{
+		(*list)->prev = node;
+		node->next = *list;
+		*list = node;
+	}
+}
