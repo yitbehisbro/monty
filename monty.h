2c40f4d5f7cab9ea821cb51bb9f1ac76939def0a
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-05 17:36:10 +0300

    Updates

--- monty.h
+++ monty.h
@@ -75,6 +75,6 @@ void _rotr(stack_t **stack, unsigned int count_line);
 /*************stack_queue.c*********************/
 void _stack(stack_t **stack, unsigned int count_line);
 void _queue(stack_t **stack, unsigned int count_line);
-void reverse(stack_t **head_ref);
+stack_t *reverse(stack_t **head_ref);
 
 #endif
