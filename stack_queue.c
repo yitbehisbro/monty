5f248398abb3e51b02b19c7436a7bb74bf5e7e2e
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-05 17:01:06 +0300

    Updates

--- stack_queue.c
+++ stack_queue.c
@@ -21,3 +21,32 @@ void _stack(stack_t **list, unsigned int count)
 		node = node->next;
 	}
 }
+
+/**
+ * _queue - sets the format of the data to a queue (FIFO).
+ * @head: pointer to struct
+ * @count: number of line number
+ *
+ * Return: no return
+ *
+ */
+void _queue(stack_t **head, unsigned int count)
+{
+	listint_t *prev, *next;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	if ((*head)->next == NULL)
+		return (*head);
+
+	prev = NULL;
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+}
