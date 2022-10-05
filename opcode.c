a139f5f91474c3ac9f5689de4254a24bf14e5da3
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-05 13:22:10 +0300

    Opcode operation file created

---
+++ opcode.c
@@ -0,0 +1,81 @@
+#include "monty.h"
+
+static instruction_t ops[] = {
+	{"push", _push},
+	{"pall", _pall},
+	{NULL, NULL}
+};
+
+/**
+ * call - call the particular function
+ * @opcode_tokens: tokens of opcode
+ * @stack: pointer to stack_t
+ *
+ * Return: void has nothing to return
+ */
+void call(char **opcode_tokens, stack_t **stack)
+{
+	int i = 0;
+
+	while (ops[i].opcode)
+	{
+		if (opcode_tokens[0][0] == '#')
+			return;
+		if (strcmp(opcode_tokens[0], ops[i].opcode) == 0)
+		{
+			if (ops[i].f)
+				ops[i].f(stack, line_number);
+			break;
+		}
+		i++;
+	}
+
+	if (strcmp(opcode_tokens[0], "push") == 0)
+	{
+		is_token(opcode_tokens, stack);
+		(*stack)->n = atoi(opcode_tokens[1]);
+	}
+	else if (!(ops[i].opcode))
+	{
+		fprintf(stderr, "L%u: unknown instruction %s\n",
+			line_number,
+			opcode_tokens[0]);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * is_token - check if a token is valid
+ * @opcode_tokens: opcode tokens
+ * @stack: pointer to a stack_t
+ *
+ * Return: void has no return
+ */
+void is_token(char **opcode_tokens, stack_t **stack)
+{
+	int i = 0;
+
+	if (!opcode_tokens[1])
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		if (*stack)
+			free_stack(stack);
+		free(opcode_tokens);
+		exit(EXIT_FAILURE);
+	}
+
+	while (opcode_tokens[1][i])
+	{
+		if (opcode_tokens[1][i] == '-' && i == 0)
+			i++;
+		if (isdigit(opcode_tokens[1][i]) == 0)
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line_number);
+			if (*stack)
+				free_stack(stack);
+			free(opcode_tokens);
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+}
