af2434e963d242bfcda75272affe62eae9c4391e
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-10-06 14:05:47 +0300

    Updates

--- opcode.c
+++ opcode.c
@@ -1,7 +1,5 @@
 #include "monty.h"
 
-unsigned int status;
-
 static instruction_t ops[] = {
 	{"push", _push}, {"pall", _pall}, {"pint", _pint}, {"pop", _pop},
 	{"swap", _swap}, {"add", _add}, {"nop", NULL}, {"sub", _sub}, {"div", _div},
